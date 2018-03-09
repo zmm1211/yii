ab535608636cf01d95bcbebc4176d5d9bc8fe57a
rambo <yangzongfang@100tal.com>
2018-03-09 18:51:01 +0800

    初始化yii

---
+++ controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangzongfang
+ * Date: 2018/3/9
+ * Time: 18:39
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\rest\Action;
+class TestController extends Controller{
+
+    public function actionIndex()
+    {
+        echo "yangzongfang";
+    }
+}
